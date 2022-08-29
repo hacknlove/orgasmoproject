355e8e250a0a8ef78c7cc155d92b81d3a2c1dad4
hacknlove <antonio@hacknlove.org>
2022-08-29 21:24:52 +0200

    test: working on getPageCacheKeys

---
+++ orgasmo/server/getServerSideProps/getPageCacheKeys.test.ts
@@ -0,0 +1,51 @@
+import getPageCacheKeys from "./getPageCacheKeys";
+
+jest.mock("../events", () => ({
+  __esModule: true,
+  default: {
+    emit: jest.fn(),
+  },
+}));
+
+describe("getPageCacheKeys", () => {
+  const ctx = {
+    params: ["foo"],
+    req: {
+      user: {
+        roles: ["test-role"],
+      },
+    },
+    driver: {
+      page: {
+        allParameterMethods: jest.fn(),
+      },
+      paramsMethod1(ctx) {
+        return {
+          p: ctx.params,
+        };
+      },
+      paramsMethod2(ctx) {
+        return {
+          r: ctx.req.user.roles,
+        };
+      },
+    },
+  };
+
+  it("returns an async iterable that yields the response of allParameterMethods serialized + the default params serialized", async () => {
+    ctx.driver.page.allParameterMethods.mockResolvedValue([
+      "paramsMethod1",
+      "paramsMethod2",
+    ]);
+
+    const expected = [
+      "(Hp_Jfoo",
+      "(Hr_Ptest-role",
+      "(Mparams_Jfoo.Lroles_Ptest-role",
+    ];
+
+    for await (const key of getPageCacheKeys(ctx)) {
+      expect(key).toBe(expected.shift());
+    }
+  });
+});
--- orgasmo/server/getServerSideProps/getPageCacheKeys.ts
+++ orgasmo/server/getServerSideProps/getPageCacheKeys.ts
@@ -1,9 +1,23 @@
 import { cencode } from "cencode";
-export default async function* getPageCacheKeys({ driver, ctx }) {
-  const methods = (await driver.page.allParameterMethods?.(ctx)) ?? [];
-  for (const method of methods) {
-    yield cencode(await driver[method](ctx));
+import events from "../events";
+
+export default async function* getPageCacheKeys(ctx) {
+  try {
+    const methods = await ctx.driver.page.allParameterMethods?.(ctx);
+
+    if (methods) {
+      for (const method of methods) {
+        yield cencode(await ctx.driver[method](ctx));
+      }
+    }
+  } catch (error) {
+    events.emit("error", {
+      type: "driver",
+      method: "page.allParameterMethods",
+      params: [ctx],
+      error,
+    });
   }
 
-  yield [cencode({ params: ctx.params, roles: ctx.req.user.roles })];
+  yield cencode({ params: ctx.params, roles: ctx.req.user.roles });
 }
