d2232846d3cbd868aa9b4f38fc17135f603c4b80
hacknlove <antonio@hacknlove.org>
2022-09-01 08:31:39 +0200

    fix: e2e driver updated to met latest changes

--- e2e/drivers/mocked/page/getPageFromId.export.js
+++ e2e/drivers/mocked/page/getPageFromId.export.js
@@ -1,5 +1,5 @@
 import pages from './pages'
 
 export default function getPageFromId(config) {
-    return pages[config.pageId]?.page
+    return pages[config.pageId]
 }
--- e2e/drivers/mocked/page/pages.js
+++ e2e/drivers/mocked/page/pages.js
@@ -2,122 +2,114 @@ import getRow from "../verticalscroll/getRow.export"
 
 export default {
     '/': {
-        page: {
-            id: '/',
-            top: [
-                {
-                    type: 'Header',
-                    props: {
-                        text: 'Home recargar'
-                    }
+        id: '/',
+        top: [
+            {
+                type: 'Header',
+                props: {
+                    text: 'Home recargar'
                 }
-            ],
-            rows: [
-                {
-                    type: 'Row',
-                    props: {
-                        title: 'Row #1'
-                    },
-                    getProps: 'random.rows',
-                    seed: 'first'
+            }
+        ],
+        rows: [
+            {
+                type: 'Row',
+                props: {
+                    title: 'Row #1'
                 },
-                {
-                    type: 'Row',
-                    props: {
-                        title: 'Row #2'
-                    },
-                    getProps: 'random.rows',
-                    seed: 'second'
+                getProps: 'random.rows',
+                seed: 'first'
+            },
+            {
+                type: 'Row',
+                props: {
+                    title: 'Row #2'
                 },
-                {
-                    type: 'Row',
-                    props: {
-                        title: 'Row #3'
-                    },
-                    getProps: 'random.rows',
-                    seed: 'third'
+                getProps: 'random.rows',
+                seed: 'second'
+            },
+            {
+                type: 'Row',
+                props: {
+                    title: 'Row #3'
                 },
-                
-            ],
-            getRow: 'home.getRow',
-            rowsLimit: 2,
-            bottom: [
-                {
-                    type: 'Footer'
-                }
-            ]
-        }
+                getProps: 'random.rows',
+                seed: 'third'
+            },
+            
+        ],
+        getRow: 'home.getRow',
+        rowsLimit: 2,
+        bottom: [
+            {
+                type: 'Footer'
+            }
+        ]
     },
     basiclayout: {
-        page: {
-            id: 'basiclayout',
-            top: [
-                {
-                    type: 'Header',
-                    props: {
-                        text: 'Basic Layout test'
-                    }
+        id: 'basiclayout',
+        top: [
+            {
+                type: 'Header',
+                props: {
+                    text: 'Basic Layout test'
                 }
-            ],
-            rows: [
-                {
-                    type: 'Item',
-                    props: {
-                        string: 'Hello world',
-                        number: 5,
-                        className: 'fullWidth'
-                    },
+            }
+        ],
+        rows: [
+            {
+                type: 'Item',
+                props: {
+                    string: 'Hello world',
+                    number: 5,
+                    className: 'fullWidth'
                 },
-                
-            ],
-            bottom: [
-                {
-                    type: 'Footer'
-                }
-            ]
-        }
+            },
+            
+        ],
+        bottom: [
+            {
+                type: 'Footer'
+            }
+        ]
     },
     horizontalscroll: {
-        page: {
-            id: 'horizontalscroll',
-            top: [
-                {
-                    type: 'Header',
-                    props: {
-                        text: 'Horizontal Scroll test'
-                    }
+        id: 'horizontalscroll',
+        top: [
+            {
+                type: 'Header',
+                props: {
+                    text: 'Horizontal Scroll test'
                 }
-            ],
-            rows: [
-                {
-                    type: 'Row',
-                    props: {
-                        title: 'Horizontal Scroll'
-                    },
-                    getProps: 'secuential.rows',
-                }
-            ],
-        }
+            }
+        ],
+        rows: [
+            {
+                type: 'Row',
+                props: {
+                    title: 'Horizontal Scroll'
+                },
+                getProps: 'secuential.rows',
+            }
+        ],
     },
     verticalscroll: {
-        page: {
-            id: 'verticalscroll',
-            top: [
-                {
-                    type: 'Header',
-                    props: {
-                        text: 'Vertical Scroll test'
-                    }
+        id: 'verticalscroll',
+        top: [
+            {
+                type: 'Header',
+                props: {
+                    text: 'Vertical Scroll test'
                 }
-            ],
-            rows: [
-                getRow({ number: 0 }),
-                getRow({ number: 1 }),
-                getRow({ number: 2 }),
-                getRow({ number: 3 }),
-            ],
-            rowsLimit: 4,
-            getRow: 'verticalscroll.getRow',
-        }
+            }
+        ],
+        rows: [
+            getRow({ number: 0 }),
+            getRow({ number: 1 }),
+            getRow({ number: 2 }),
+            getRow({ number: 3 }),
+        ],
+        rowsLimit: 4,
+        getRow: 'verticalscroll.getRow',
     },
 }
