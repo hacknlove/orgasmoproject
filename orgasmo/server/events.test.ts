d5570ae22f24c5beac2df8bd744d42ddef9b9f75
hacknlove <antonio@hacknlove.org>
2022-09-01 09:06:10 +0200

    prettylint

--- orgasmo/server/events.test.ts
+++ orgasmo/server/events.test.ts
@@ -1,6 +1,6 @@
-import { EventEmitter } from 'node:stream'
-import events from './events'
+import { EventEmitter } from "node:stream";
+import events from "./events";
 
-test('exports an event emiter', () => {
-    expect(events).toBeInstanceOf(EventEmitter)
-})
+test("exports an event emiter", () => {
+  expect(events).toBeInstanceOf(EventEmitter);
+});
