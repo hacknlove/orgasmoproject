f082f5368e5f9e656ac1e31ef14f88f65f372339
hacknlove <antonio@hacknlove.org>
2022-09-24 10:54:47 +0200

    e2e test

--- create-orgasmo/empty/components/Layout.dynamic.jsx
+++ create-orgasmo/empty/components/Layout.dynamic.jsx
@@ -1,20 +1,18 @@
 import Head from "next/head";
 
-export default function Layout({ header, main, footer, meta }) {
+export default function Layout({ areas }) {
   return (
     <div className="container">
       <Head>
-        <title>{meta.title}</title>
-        <meta name="description" content={meta.description} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="main">
-        {header}
-        <div className="grid">{main}</div>
+        {areas.header}
+        <div className="grid">{areas.main}</div>
       </main>
 
-      <footer className="footer">{footer}</footer>
+      <footer className="footer">{areas.footer}</footer>
     </div>
   );
 }
