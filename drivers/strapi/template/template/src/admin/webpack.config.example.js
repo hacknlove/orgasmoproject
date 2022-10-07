b5b218d7556f475a6a44b0e79326e7c9830a0d17
hacknlove <antonio@hacknlove.org>
2022-10-07 13:31:44 +0200

    eslint

--- drivers/strapi/template/template/src/admin/webpack.config.example.js
+++ drivers/strapi/template/template/src/admin/webpack.config.example.js
@@ -1,9 +1,5 @@
 "use strict";
 
-/* eslint-disable no-unused-vars */
-module.exports = (config, webpack) => {
-  // Note: we provide webpack above so you should not `require` it
-  // Perform customizations to webpack config
-  // Important: return the modified config
+module.exports = (config) => {
   return config;
 };
