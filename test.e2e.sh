9a6f53ea649a909243db04aaa33f9ff9507c197c
hacknlove <antonio@hacknlove.org>
2022-10-13 08:43:18 +0200

    fix: build order

--- test.e2e.sh
+++ test.e2e.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-npm run build -w orgasmo  -w drivers/json -w admin
+npm run build -w orgasmo -w drivers/json -w state -w admin
 
 npm test -w examples/infinite
 npm test -w create-orgasmo
