b60487c02cc55bb362080f9fa1e292cbb921a56c
hacknlove <antonio@hacknlove.org>
2022-10-24 19:43:54 +0200

    update mongo driver

---
+++ drivers/mongo/src/admin/getComponentStory.export.ts
@@ -0,0 +1,14 @@
+import mongoProxy from "../mongoProxy";
+
+const storyConfigsCollectionName =
+  (process.env.ORGASMO_MONGO_STORIES_COLLECTION as string) ?? "storyConfigs";
+
+export default async function getComponentStory({ component, story }) {
+  console.log({ component, story })
+  await mongoProxy.connect();
+
+  return mongoProxy[storyConfigsCollectionName].findOne({
+    story,
+    'itemConfig.type': component
+  })
+}
--- drivers/mongo/src/driver.ts
+++ drivers/mongo/src/driver.ts
@@ -1,32 +1,40 @@
+
 import adminーdeletePageConfig from "./admin/deletePageConfig.export";
 import adminーdeleteStoryConfig from "./admin/deleteStoryConfig.export";
 import adminーgetAllPages from "./admin/getAllPages.export";
 import adminーgetAllStories from "./admin/getAllStories.export";
+import adminーgetComponentStory from "./admin/getComponentStory.export";
 import adminーnewPageConfig from "./admin/newPageConfig.export";
 import adminーupdatePageConfig from "./admin/updatePageConfig.export";
 import adminーupsertPageConfig from "./admin/upsertPageConfig.export";
 import adminーupsertStoryConfig from "./admin/upsertStoryConfig.export";
 import pageーgetPageConfig from "./page/getPageConfig.export";
 import pageーgetPageConfigFromId from "./page/getPageConfigFromId.export";
+import siteーgetConfig from "./site/getConfig.export";
+
+
 
 const driver = {
   ["admin.deletePageConfig"]: adminーdeletePageConfig,
   ["admin.deleteStoryConfig"]: adminーdeleteStoryConfig,
   ["admin.getAllPages"]: adminーgetAllPages,
   ["admin.getAllStories"]: adminーgetAllStories,
+  ["admin.getComponentStory"]: adminーgetComponentStory,
   ["admin.newPageConfig"]: adminーnewPageConfig,
   ["admin.updatePageConfig"]: adminーupdatePageConfig,
   ["admin.upsertPageConfig"]: adminーupsertPageConfig,
   ["admin.upsertStoryConfig"]: adminーupsertStoryConfig,
   ["page.getPageConfig"]: pageーgetPageConfig,
   ["page.getPageConfigFromId"]: pageーgetPageConfigFromId,
-};
+  ["site.getConfig"]: siteーgetConfig,
+}
 
 driver["admin"] = {};
 driver["admin"]["deletePageConfig"] = adminーdeletePageConfig;
 driver["admin"]["deleteStoryConfig"] = adminーdeleteStoryConfig;
 driver["admin"]["getAllPages"] = adminーgetAllPages;
 driver["admin"]["getAllStories"] = adminーgetAllStories;
+driver["admin"]["getComponentStory"] = adminーgetComponentStory;
 driver["admin"]["newPageConfig"] = adminーnewPageConfig;
 driver["admin"]["updatePageConfig"] = adminーupdatePageConfig;
 driver["admin"]["upsertPageConfig"] = adminーupsertPageConfig;
@@ -34,5 +42,7 @@ driver["admin"]["upsertStoryConfig"] = adminーupsertStoryConfig;
 driver["page"] = {};
 driver["page"]["getPageConfig"] = pageーgetPageConfig;
 driver["page"]["getPageConfigFromId"] = pageーgetPageConfigFromId;
+driver["site"] = {};
+driver["site"]["getConfig"] = siteーgetConfig;
 
 export default driver;
